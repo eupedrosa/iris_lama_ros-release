ac3209594ef3ddb25b34142233cabee869c352a6
Eurico F. Pedrosa <efp@ua.pt>
2021-04-10 15:59:55 +0100

    Rebase from 'release/melodic/iris_lama_ros'

--- include/lama/ros/loc2d_ros.h
+++ include/lama/ros/loc2d_ros.h
@@ -91,6 +91,8 @@ private:
     // Trigger a global localization procedure.
     bool globalLocalizationCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& resp);
 
+    void publishCurrentPose();
+
 private:
 
     // == ROS stuff ==
@@ -120,7 +122,6 @@ private:
     // == Laser stuff ==
     // Handle multiple lasers at once
     std::map<std::string, int> frame_to_laser_; ///< Map with the known lasers.
-    std::vector<bool>          laser_is_reversed_;;  ///< Vector that signals if the laser is reversed
     std::vector<Pose3D>        lasers_origin_;  ///< Laser origin transformation
 
     // == configuration variables ==
@@ -142,6 +143,8 @@ private:
     Pose2D odom_;
     Loc2D::Options options_;
     Pose2D initial_prior_;
+    tf::Quaternion current_orientation_;
+    geometry_msgs::PoseWithCovarianceStamped cur_pose_msg_;
 };
 
 } /* lama */
